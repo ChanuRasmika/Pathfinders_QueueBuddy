1f94c7e09e4c23c6527c2de8bc8c79a9920697bd
ChanuRasmika <chanurashmika.edu@gmail.com>
2025-08-16 22:50:49 +0530

    fiexd  bugs

--- gov_officer_portal/src/main.jsx
+++ gov_officer_portal/src/main.jsx
@@ -1,19 +1,17 @@
-import { StrictMode } from "react";
+import React from "react";
 import { createRoot } from "react-dom/client";
-import { BrowserRouter } from "react-router-dom";
-import './index.css';
-import GlobalStyle from './globalStyles.js';
-
-import GovernmentLogin from "./Components/government-login.jsx";
-import DasboardHome from "./Components/dashboard.jsx";
-
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import "./style.css";
+import GovernmentLogin from "../src/government-login.jsx";
+import DashboardHome from "../src/dashboard.jsx";
 
 createRoot(document.getElementById("root")).render(
-  <StrictMode>
-    <BrowserRouter>
-      <GlobalStyle />
-      <GovernmentLogin />
-      <DasboardHome />
-    </BrowserRouter>
-  </StrictMode>
+    <React.StrictMode>
+        <BrowserRouter>
+            <Routes>
+                <Route path="/" element={<GovernmentLogin />} />
+                <Route path="/dashboard" element={<DashboardHome />} />
+            </Routes>
+        </BrowserRouter>
+    </React.StrictMode>
 );
