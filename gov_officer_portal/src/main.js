a3e537dd1b9db8f964d79276a2674679d1980569
Geeth Seneviratne <140522569+geethsenaviratne@users.noreply.github.com>
2025-08-16 22:07:51 +0530

    Update main.js

--- gov_officer_portal/src/main.js
+++ gov_officer_portal/src/main.js
@@ -1,24 +1,19 @@
-import './style.css'
-import javascriptLogo from './javascript.svg'
-import viteLogo from '/vite.svg'
-import { setupCounter } from './counter.js'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import { BrowserRouter } from "react-router-dom";
+import './index.css';
+import GlobalStyle from './globalStyles.js';
 
-document.querySelector('#app').innerHTML = `
-  <div>
-    <a href="https://vite.dev" target="_blank">
-      <img src="${viteLogo}" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://developer.mozilla.org/en-US/docs/Web/JavaScript" target="_blank">
-      <img src="${javascriptLogo}" class="logo vanilla" alt="JavaScript logo" />
-    </a>
-    <h1>Hello Vite!</h1>
-    <div class="card">
-      <button id="counter" type="button"></button>
-    </div>
-    <p class="read-the-docs">
-      Click on the Vite logo to learn more
-    </p>
-  </div>
-`
+import GovernmentLogin from "./Components/government-login.jsx";
+import DasboardHome from "./Components/dashboard.jsx";
 
-setupCounter(document.querySelector('#counter'))
+
+createRoot(document.getElementById("root")).render(
+  <StrictMode>
+    <BrowserRouter>
+      <GlobalStyle />
+      <GovernmentLogin />
+      <DasboardHome />
+    </BrowserRouter>
+  </StrictMode>
+);
